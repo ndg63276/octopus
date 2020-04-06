0834b715f450ed698ecaf6d62168599bb5f8a9ec
Mark Williams <24956497+ndg63276@users.noreply.github.com>
2020-04-06 10:32:03 +0100

    Dont upload images

--- scripts/s3upload.sh
+++ scripts/s3upload.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 sync . s3://smartathome.co.uk/octopus/ --exclude ".git*" --exclude "scripts/*"
+aws s3 sync . s3://smartathome.co.uk/octopus/ --exclude ".git*" --exclude "scripts/*" --exclude "images/*"
